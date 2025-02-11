3339f6cf6890d64091519a6e3d27f70c8ab2889e
Iván <72264031+xIvqn@users.noreply.github.com>
2025-02-11 19:00:20 +0100

    feat: problems endpoints

--- src/pydomjudge/client.py
+++ src/pydomjudge/client.py
@@ -4,7 +4,7 @@ import requests
 from requests.auth import HTTPDigestAuth
 
 from pydomjudge.models.main import Contest, Clarification, Submission, User, Award, Balloon, ContestState, \
-    ContestStatus, Event
+    ContestStatus, Event, ContestProblem
 from pydomjudge.models.request import ClarificationPost
 from pydomjudge.models.response import Scoreboard, AccessInformation
 from pydomjudge.models.shared import ArchiveFile, SourceCode
@@ -330,6 +330,70 @@ class DOMJudge:
         response = self.session.get(url, params=params)
         response.raise_for_status()
         return response.content
+
+    # Problems section
+    def get_all_problems(self, contest_id: Union[str, int], idlist: List[str] = None, strict: bool = False) -> List[ContestProblem]:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems"
+        params = {
+            "ids[]": idlist,
+            "strict": strict
+        }
+        response = self.session.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def get_problem(self, contest_id: Union[str, int], problem_id: str, strict: bool = False) -> ContestProblem:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems/{problem_id}"
+        params = {
+            "strict": strict
+        }
+        response = self.session.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def add_problem(self, contest_id: Union[str, int], problem_data: dict, strict: bool = False) -> str:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems"
+        params = {
+            "strict": strict
+        }
+        response = self.session.post(url, params=params, files=problem_data)
+        response.raise_for_status()
+        return response.json()
+
+    def link_problem(self, contest_id: Union[str, int], problem_id: str, problem_data: dict, strict: bool = False) -> ContestProblem:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems/{problem_id}"
+        params = {
+            "strict": strict
+        }
+        response = self.session.put(url, params=params, json=problem_data)
+        response.raise_for_status()
+        return response.json()
+
+    def unlink_problem(self, contest_id: Union[str, int], problem_id: str, strict: bool = False) -> None:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems/{problem_id}"
+        params = {
+            "strict": strict
+        }
+        response = self.session.delete(url, params=params)
+        response.raise_for_status()
+
+    def get_problem_statement(self, contest_id: Union[str, int], problem_id: str, strict: bool = False) -> bytes:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems/{problem_id}/statement"
+        params = {
+            "strict": strict
+        }
+        response = self.session.get(url, params=params)
+        response.raise_for_status()
+        return response.content
+
+    def add_problems(self, contest_id: Union[str, int], problems_data: dict, strict: bool = False) -> List[str]:
+        url = f"{self.base_url}/api/v4/contests/{contest_id}/problems/add-data"
+        params = {
+            "strict": strict
+        }
+        response = self.session.post(url, params=params, files=problems_data)
+        response.raise_for_status()
+        return response.json()
 
     def get_scoreboard(self, contest_id: Union[str, int], allteams: bool = None, category: Union[str, int] = None,
                        country: str = None, affiliation: Union[str, int] = None, public: bool = None,
