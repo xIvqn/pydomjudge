4c7b25d8a073aff8568477826111e522c350c497
Iván <72264031+xIvqn@users.noreply.github.com>
2025-02-11 18:41:55 +0100

    fix: auth to digest

--- src/pydomjudge/client.py
+++ src/pydomjudge/client.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import requests
-from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPDigestAuth
 
 from pydomjudge.models.main import Contest, Clarification
 from pydomjudge.models.request import ClarificationPost
@@ -10,21 +10,19 @@ from pydomjudge.models.request import ClarificationPost
 class DOMJudge:
     def __init__(self, base_url: str, username: str, password: str):
         self.base_url = base_url
-        self.auth = HTTPBasicAuth(username, password)
+        self.session = requests.Session()
+        self.session.auth = HTTPDigestAuth(username, password)
+
 
     def get_contests(self) -> List[Contest]:
-        response = requests.get(f"{self.base_url}/api/v4/contests", auth=self.auth)
-        data = response.json()
-
-        if response.status_code != 200:
-            raise requests.exceptions.HTTPError(f"{response.status_code} Client Error: {data.get('error', '')}")
-
+        response = self.session.get(f"{self.base_url}/api/v4/contests")
+        response.raise_for_status()
         return [Contest(**item) for item in response.json()]
 
-    def submit_clarification(self, cid: str, data: ClarificationPost) -> Clarification:
-        response = requests.post(
+    def submit_clarification(self, cid: Union[str, int], data: ClarificationPost) -> Clarification:
+        response = self.session.post(
             f"{self.base_url}/api/v4/contests/{cid}/clarifications",
-            json=data.dict(),
-            auth=self.auth
+            json=data.dict()
         )
+        response.raise_for_status()
         return Clarification(**response.json())
