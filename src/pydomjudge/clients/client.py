d9c291ecd037741abb0e8050289505e0201cdbec
Iván <72264031+xIvqn@users.noreply.github.com>
2025-09-17 10:17:06 +0200

    fix: migrate to basic auth

--- src/pydomjudge/clients/client.py
+++ src/pydomjudge/clients/client.py
@@ -1,9 +1,9 @@
 import requests
-from requests.auth import HTTPDigestAuth
+from requests.auth import HTTPBasicAuth
 
 
 class _Client:
     def __init__(self, base_url: str, username: str, password: str):
         self.base_url = base_url
         self.session = requests.Session()
-        self.session.auth = HTTPDigestAuth(username, password)
+        self.session.auth = HTTPBasicAuth(username, password)
